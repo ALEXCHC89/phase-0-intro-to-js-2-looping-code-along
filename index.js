4d9fcd1ff8080e91f4a9e4377d7d0f2b159e0dce
Alexon <alexchc89@gmail.com>
2021-03-20 07:39:55 -0700

    Done with assignment (looping code)

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+const celeb = "birthday";
+function writeCards(names, celeb) {
+  const cardMessages = []
+  for (let i = 0; i < names.length; i++) {
+    const Note = `Thank you, ${names[i]}, for the wonderful ${celeb} gift!`;
+    cardMessages.push(Note);
+  }
+  return cardMessages;
+}
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i)
+  }
+}
